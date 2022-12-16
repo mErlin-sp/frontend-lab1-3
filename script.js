02b1d09ec7f3fe08dee359f88e77403656b70326
merlin-sp <merlin.ua.1@gmail.com>
2022-12-17 00:49:34 +0200

    Implement tasks from lab4

---
+++ script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const study = document.getElementById('study');
+
+    study.onclick = function () {
+        if (study.getAttribute('class') === 'preset1') {
+            study.setAttribute('class', 'preset2')
+        } else {
+            study.setAttribute('class', 'preset1')
+        }
+    }
+
+    const hobbies = document.querySelector('#hobbies')
+
+    hobbies.onclick = function () {
+        if (hobbies.getAttribute('class') === 'preset1') {
+            hobbies.setAttribute('class', 'preset2')
+        } else {
+            hobbies.setAttribute('class', 'preset1')
+        }
+    }
+
+    $('#add-img-button').click(function () {
+        $('#images').children().each(function () {
+            if ($(this).css('display') === 'none') {
+                $(this).css('display', 'block')
+                return false
+            }
+        })
+    });
+
+    $('#rm-img-button').click(function () {
+        $($('#images').children().toArray().reverse()).each(function () {
+            if ($(this).css('display') !== 'none') {
+                $(this).css('display', 'none')
+                return false
+            }
+        })
+    });
+
+    $('#zoom-in-button').click(function () {
+        $($('#images').children().toArray().reverse()).each(function () {
+            if ($(this).css('display') !== 'none') {
+                $(this).children().first().width($(this).children().first().width() * 1.1)
+                $(this).children().first().height($(this).children().first().height() * 1.1)
+                return false
+            }
+        })
+    });
+
+    $('#zoom-out-button').click(function () {
+        $($('#images').children().toArray().reverse()).each(function () {
+            if ($(this).css('display') !== 'none') {
+                $(this).children().first().width($(this).children().first().width() * 0.9)
+                $(this).children().first().height($(this).children().first().height() * 0.9)
+                return false
+            }
+        })
+    });
+});
+
